e4cb2f4d6d9cb169e5d6f5f9ffc86eb021cf6f8c
Dishant <dishantpatel27@gmail.com>
2017-11-06 21:13:53 +0000

    Enumeration-with-Candidates-Ruby-

--- filters.rb
+++ filters.rb
@@ -3,14 +3,34 @@
 
 def find(id)
   # Your code Here
+ @candidates.select{|singleCandidate| singleCandidate[:id] == id}
 end
 
 def experienced?(candidate)
   # Your code Here
+  if candidate[:years_of_experience] >= 2
+    true
+  else 
+    false
+  end
 end
 
 def qualified_candidates(candidates)
   # Your code Here
+  candidates.select{|singleCandidate| qualified_helper(singleCandidate)} 
 end
 
 # More methods will go below
+
+def qualified_helper(singleCandidate)
+  if(singleCandidate[:years_of_experience] != 0 && singleCandidate[:github_points] >= 100 && (singleCandidate[:languages].include?("Python") || singleCandidate[:languages].include?("Ruby")) && singleCandidate[:date_applied] >= 15.days.ago.to_date && singleCandidate[:age] > 17)
+    true
+  else
+    false
+  end
+end
+
+
+def ordered_by_qualifications(candidates)
+  candidates.sort{|a,b|  (a[:years_of_experience] <=> b[:years_of_experience]) == 0 ? a[:github_points] <=> b[:github_points]:a[:years_of_experience] <=> b[:years_of_experience] }.reverse
+end
